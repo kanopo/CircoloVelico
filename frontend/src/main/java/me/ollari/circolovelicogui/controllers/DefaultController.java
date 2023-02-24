08d9e9cbe21ce12d84d743178025a9157ab18040
kanopo <dmitri.ollari@protonmail.com>
2023-02-24 11:36:37 +0100

    Added auto creation of admin users at first start

--- frontend/src/main/java/me/ollari/circolovelicogui/controllers/DefaultController.java
+++ frontend/src/main/java/me/ollari/circolovelicogui/controllers/DefaultController.java
@@ -9,10 +9,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import me.ollari.circolovelicogui.HttpFunctions;
 
+import java.io.IOException;
+import java.net.http.HttpResponse;
 import java.util.Objects;
 
+
+
 public class DefaultController {
+    private HttpFunctions httpFunctions = new HttpFunctions();
+
     public VBox root;
     public Button login_member_btn;
     public Button login_employee_btn;
@@ -38,6 +45,38 @@ public class DefaultController {
     }
 
     public void login_employee(ActionEvent actionEvent) {
+
+        HttpResponse<String> response = null;
+        try {
+            response = httpFunctions.GET("/employee/username/admin");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        if (response.statusCode() == 200) {
+            // admin già registrato nel db
+        }else {
+            // admin assente dal db
+            // Aggiungo admin
+
+            String body = "{\"username\":\"" + "admin" + "\",\"password\":\"" + "password"+ "\"}";
+            System.out.println(body);
+            HttpResponse<String> responsePost = null;
+            try {
+                 responsePost = httpFunctions.POST("/employee", body);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(responsePost);
+        }
+
+
+
+
         try {
             Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/employeeLogin.fxml")));
 
